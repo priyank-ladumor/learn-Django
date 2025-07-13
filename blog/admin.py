78d362af452f0b32c4bf4625e81dacbb7834b8eb
priyank-ladumor-2003 <priyank.suratinfotech@gmail.com>
2025-07-13 14:52:22 +0530

    blog relation models add and customize admin register

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,46 @@
-from django.contrib import admin
-from .models import Blog
 
-# Register your models here.
-admin.site.register(Blog)
+from django.contrib import admin
+from .models import Blog, Tag, BlogComment # . means current directory & .models means models.py
+
+# # Simple registration of models without any customizations
+# admin.site.register(Blog)
+# admin.site.register(Tag)
+# admin.site.register(BlogComment)
+
+
+# Inline model to show comments inside Blog admin page
+class BlogCommentInline(admin.TabularInline): # TabularInline is the default inline form layout & can be replaced with StackedInline for a different layout
+    model = BlogComment
+    extra = 1  # Show 1 extra blank comment inline
+
+
+# Custom Blog admin
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'blog_type', 'draft')  # Columns shown in list view
+    list_filter = ('blog_type', 'draft', 'tags')                # Sidebar filters
+    search_fields = ('title', 'description')                    # Search bar
+    filter_horizontal = ('tags',)                               # UI for many-to-many field tags on Blog admin form
+    inlines = [BlogCommentInline]                               # Show related comments inline
+
+
+# Custom Tag admin
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag_name',)
+    search_fields = ('tag_name',)
+
+
+# Custom BlogComment admin
+@admin.register(BlogComment)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'blog', 'date') # Columns shown in list view
+    # relationModelName__fieldName ex: blog__title # raverses the ForeignKey to User model and searches the username field. This is called Django ORM lookups or field traversal. 
+    search_fields = ('comment', 'user__username', 'blog__title')  # Search by comment, username, or blog title & other modals value
+    list_filter = ('date',) # Sidebar filter
+
+
+#other way to register
+# admin.site.register(Blog, BlogAdmin)
+# admin.site.register(Tag, TagAdmin)
+# admin.site.register(BlogComment, BlogCommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,69 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# ----------------------------
+# Tag Model
+# ----------------------------
+# This model represents tags that can be associated with multiple blog posts.
+# Many-to-Many relationship with Blog model:
+# - A blog can have multiple tags.
+# - A tag can belong to multiple blogs.
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=50, unique=True)  # Each tag must be unique
 
+    def __str__(self):
+        return self.tag_name
+
+
+# ----------------------------
+# Blog Model
+# ----------------------------
+# Represents the main blog post.
+# Includes a Many-to-Many relationship with Tag model,
+# and a One-to-Many relationship with BlogComment (via related_name).
 class Blog(models.Model):
+    # Choices for blog visibility or status
     BLOG_TYPE = [
         ('Public', 'Public'),
         ('Private', 'Private'),
         ('Draft', 'Draft'),
     ]
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
-    image = models.ImageField(upload_to='images/')
-    date = models.DateTimeField(default=timezone.now)
-    draft = models.BooleanField(default=False)
-    blog_type = models.CharField(max_length=7, choices=BLOG_TYPE, default='Public')
-    description = models.TextField(default='', blank=True)
-    
+
+    title = models.CharField(max_length=200)  # Blog title
+    pub_date = models.DateTimeField()         # Publication date
+    image = models.ImageField(upload_to='images/')  # Image uploaded to 'images/' directory
+    date = models.DateTimeField(default=timezone.now)  # Created date (default: now)
+    draft = models.BooleanField(default=False)         # Flag to mark blog as draft
+    blog_type = models.CharField(max_length=7, choices=BLOG_TYPE, default='Public')  # Type of blog
+    description = models.TextField(default='', blank=True)  # Optional description/content
+
+    # Many-to-Many relation with Tag model 
+    tags = models.ManyToManyField(Tag, related_name='blogs', blank=True)
+
     def __str__(self):
         return self.title
+
+
+# ----------------------------
+# BlogComment Model
+# ----------------------------
+# Represents comments made on blog posts.
+# One-to-Many relationship:
+# - A blog can have many comments.
+# - A comment belongs to one blog.
+# Also links each comment to a specific User.
+class BlogComment(models.Model):
+    # ForeignKey to Blog model:
+    # on_delete=models.CASCADE: if the blog is deleted, all related comments are also deleted.
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+
+    # ForeignKey to built-in User model:
+    # on_delete=models.CASCADE: if the user is deleted, their comments are also removed.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    comment = models.TextField()  # Content of the comment
+    date = models.DateTimeField(default=timezone.now)  # Date the comment was created
+
+    def __str__(self): # Returns a string representation of the comment when not customizing the admin interface
+        return f"{self.user.username}: {self.comment}"
